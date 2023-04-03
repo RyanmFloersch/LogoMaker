266b48811737de4daae5055b9226f1431a0967c4
Ryan <ryanmfloersch@gmail.com>
2023-04-03 17:26:39 -0400

    initital

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+const inquirer = require('inquirer');
+
+
+// Prompt user for text, up to 3 letters
+// Prompt for etext color using a color keyword or hexadecimal number
+// Prompt for a sahpe using a list of circle, triangle, and square
+// Prompt for a color same way as for the text.
+// Create an svg named logo.svg
+// Svg shows an image in 300x2000
